69f8ebe8a36886e3968c9f7b784aa419a9ef43e4
sun sreng <manacambodia@outlook.com>
2017-06-05 23:19:55 +0700

    add rount

--- routes/app.js
+++ routes/app.js
@@ -4,5 +4,12 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index');
 });
+router.get('/message/:msg', function (req, res, next) {
+    res.render('node', {message: req.params.msg});
+});
+router.get('/message', function (req, res, next) {
+    var message = req.body.message;
+    res.redirect('/message/' + message);
+});
 
 module.exports = router;
