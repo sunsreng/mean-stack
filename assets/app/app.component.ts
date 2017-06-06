08940126f460c5281c2e82da9b40e32dc00f0858
sun sreng <manacambodia@outlook.com>
2017-06-06 22:01:35 +0700

    add @Input()

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Message } from './messages/message.module';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    content = 'Some content';
+    message: Message = new Message ('A message.', 'Sreng');
 }
